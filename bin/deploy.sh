22d3ab83b384439fd5f80f96ba5f85dd30ea2b78
alexperronnet <github.ayfb9@lxprnt.com>
2023-05-23 01:08:36 +0200

    build: Fix deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,9 +7,9 @@ build_command="build"
 build_dir="dist"
 default_branch="main"
 deploy_branch="deploy"
-origin_remote="origin"
-commit_message="Automatic build commit"
 commit_hash=$(git rev-parse --short HEAD)
+commit_message="Automated deployment from $commit_hash"
+remote_name="origin"
 
 # Build the project
 if ! pnpm $build_command; then
@@ -17,32 +17,34 @@ if ! pnpm $build_command; then
   exit 1
 fi
 
-# Check if deploy branch exists
-if git show-ref --verify --quiet refs/heads/$deploy_branch; then
-  # Checkout deploy branch
-  git checkout $deploy_branch
-else
-  # Create deploy branch
-  git checkout -b $deploy_branch
+# Create deploy branch if it doesn't exist
+if ! git show-ref --verify --quiet "refs/heads/$deploy_branch"; then
+  git branch $deploy_branch
 fi
 
-# Remove old build files
-git rm -r *
+# Checkout the deploy branch
+git checkout $deploy_branch
 
-# Copy build files to root
+# Remove all files not in .git or in the build directory
+find . -maxdepth 1 ! -name '.git' ! -name ".gitignore" ! -name "$build_dir" ! -name '.' -exec rm -rf {} \;
+
+# Copy the build files to the root directory
 cp -r $build_dir/* .
 
 # Add all files to git
 git add .
 
-# Commit changes
-git commit -m "$commit_message: $commit_hash"
+# Remove files that were deleted during the build
+git add -u
 
-# Push changes to deploy branch
-git push $origin_remote $deploy_branch
+# Commit the changes
+git commit -m "$commit_message"
 
-# Checkout main branch
-git checkout $default_branch
+# Push the changes to the remote
+git push $remote_name $deploy_branch
 
-# Delete dist folder
+# Checkout the previous branch
+git checkout -
+
+# Delete build directory
 rm -rf $build_dir
