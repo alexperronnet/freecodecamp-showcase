4af9db3d53690399457a88108e1eb53f5ad45f1b
alexperronnet <github.ayfb9@lxprnt.com>
2023-05-23 03:25:29 +0200

    feat: Build loader

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,1 +1,10 @@
-export const Loader = () => <div>Loader</div>
+import { Icon } from '@/components'
+
+import styles from './loader.module.scss'
+
+export const Loader = () => (
+  <div className={styles.loader}>
+    <Icon name='loader' />
+    <p>Loading...</p>
+  </div>
+)
